6e301081e9d91948f995219e91f90a24ad602d1e
julianvn <47314958+julianvn@users.noreply.github.com>
2019-05-24 12:30:34 +0200

    2.1 opdr1

---
+++ inschrijvingen.sql
@@ -0,0 +1,5 @@
+use lo8e_sql2;
+INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code)
+values ('Lo9e-amo1', "akr02", 2019, 'amo'),
+('Lo9e-amo2', "bkr02", 2019, 'amo'),
+('Lo9e-amo3', "brc01", 2019, 'amo');
