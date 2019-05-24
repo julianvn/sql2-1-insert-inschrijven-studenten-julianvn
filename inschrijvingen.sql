372eef1905061c3231b85d769c0f6e5e5aaf18dc
julianvn <47314958+julianvn@users.noreply.github.com>
2019-05-24 13:13:41 +0200

    2.1 opdr 2

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -3,3 +3,11 @@ INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code)
 values ('Lo9e-amo1', "akr02", 2019, 'amo'),
 ('Lo9e-amo2', "bkr02", 2019, 'amo'),
 ('Lo9e-amo3', "brc01", 2019, 'amo');
+
+use lo8e_sql2;
+INSERT INTO studenten(ov_nummer, inschrijvings_datum,
+ plaats, geboortedatum)
+ values('90000', '2019-01-01', 'leiden', '09-12-2000 00:00:00'),
+ ('90001', '2019-01-01', 'voorschoten', '10-08-1996 00:00:00'),
+ ('90002', '2019-01-01', 'oegstgeest', '10-09-1995 00:00:00'),
+ ('90003', '2019-01-01', 'zoetermeer', '05-12-1999 00:00:00')
